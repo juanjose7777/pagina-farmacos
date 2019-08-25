1d4047d66e9edf84adb7f22554881c13bf72bdfc
juanjose7777 <53951019+juanjose7777@users.noreply.github.com>
2019-08-25 17:46:48 -0500

    lineas principales de boostrap
    se elabora un boton, y se realiza parte de vision hasta ahora

---
+++ paginaprincipal.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>farmacos.info!</title>
+  </head>
+  <body>
+    
+    <div class="row">
+    <div class="col-12 col-md-4 col-lg-4"></div>
+    <div class="col-12 col-md-4 col-lg-4">
+    <h1>bienvenido a farmacos.info!</h1><br><br>
+     <div class="col-12 col-md-4 col-lg-4"></div>
+    
+     <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">ingresa el respectivo correo</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="ingresa el respectivo correo">
+    <small id="emailHelp" class="form-text text-muted">recuerda llenar los campos bb.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">clave papu</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="clave papu">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="btn btn-outline-success" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">sisisi</label>
+  </div>
+  <button type="submit" class="btn btn-outline-danger  btn-lg btn-block">oprimeme</button>
+</form>
+     
+
+   
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
