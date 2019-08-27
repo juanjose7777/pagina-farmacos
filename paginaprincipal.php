39529c21e0c49f1d09d1186a54237d747ad1f1df
juanjose7777 <53951019+juanjose7777@users.noreply.github.com>
2019-08-27 18:49:31 -0500

    arreglos
    arreglos y correciones

--- paginaprincipal.php
+++ paginaprincipal.php
@@ -8,11 +8,11 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <title>farmacos.info!</title>
+    <title> fármacos.info!</title>
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Farmacos.info</a>
+  <nav class="navbar navbar-expand-lg navbar-light bg-danger">
+  <a class="navbar-brand" href="#">Fármacos.info</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -20,43 +20,43 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">casa <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Casa <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Contactanos</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          herramientas
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">acciones</a>
+          <a class="dropdown-item" href="#">otras acciones</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">nolose bro</a>
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link " href="#"></a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <input class="form-control mr-sm-2" type="buscar" placeholder="buscar" aria-label="buscador">
+      <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">buscador</button>
     </form>
   </div>
 </nav>
     
     <div class="row">
     <div class="col-12 col-md-4 col-lg-5"></div>
-    <div class="col-12 col-md-4 col-lg-5">
+    <div class="col-12 col-md-4 col-lg-5"><br>
     <h1>bienvenido a farmacos.info!</h1><br><br>
      <div class="col-12 col-md-4 col-lg-5"></div>
     
      <form>
   <div class="form-group">
     <label for="exampleInputEmail1">ingresa el respectivo correo</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="ingresa el respectivo correo">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="correo">
     <small id="emailHelp" class="form-text text-muted">recuerda llenar los campos bb.</small>
   </div>
   <div class="form-group">
