fb7d820bb7a0e55baf128863f6adf367461b8e77
juanjose7777 <53951019+juanjose7777@users.noreply.github.com>
2019-08-28 21:17:42 -0500

    redireccion de boton
    se añadio la redireccion para ir a otra pagina al oprimir el boton "continuar"

---
+++ continuar.php
@@ -0,0 +1,11 @@
+<?php 
+
+//
+$usuario= $_POST["email"];
+$clave= $_POST ["pass"];
+
+echo "su usuario es $usuario <br><br>y su clave es $clave";
+
+
+
+?>
--- paginaprincipal.php
+++ paginaprincipal.php
@@ -53,21 +53,23 @@
     <h1>bienvenido a farmacos.info!</h1><br><br>
      <div class="col-12 col-md-4 col-lg-5"></div>
     
-     <form>
+     <form action="continuar.php"method="post">
   <div class="form-group">
     <label for="exampleInputEmail1">ingresa el respectivo correo</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="correo">
+    <input type="text" class="form-control" 
+         name="email"
+    id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="correo">
     <small id="emailHelp" class="form-text text-muted">recuerda llenar los campos bb.</small>
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">clave papu</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="clave papu">
+    <input type="password" class="form-control" 
+          name="pass"
+    id="exampleInputPassword1" placeholder="clave papu">
   </div>
   <div class="form-group form-check">
-    <input type="checkbox" class="btn btn-outline-success" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">sisisi</label>
+    
   </div>
-  <button type="submit" class="btn btn-outline-danger  btn-lg btn-block">oprimeme</button>
+  <button type="submit" class="btn btn-outline-danger  btn-lg btn-block">continuar</button>
 </form>
 
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
